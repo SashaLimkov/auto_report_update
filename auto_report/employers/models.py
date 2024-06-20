76efccae45b4d06bfa955aa11426c98007d6780a
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-06-20 17:14:33 +0300

    created new app Employer

---
+++ auto_report/employers/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from backend.models import TimeBasedModel
+from telegram.models import TelegramUser
+
+
+class Employer(TimeBasedModel):
+    class Meta:
+        verbose_name = "Разработчик"
+        verbose_name_plural = "Разработчики"
+        ordering = ["-user_name"]
+
+    user_name: str = models.CharField("ФИО разработчика", max_length=128)
+    tg_urls: str = models.ForeignKey(TelegramUser, on_delete=models.SET_NULL,null=True, verbose_name="Ссылка на телеграм")
+    start_time: str = models.DateTimeField("Начало рабочего дня")
+    end_time: str = models.DateTimeField("Конец рабочего дня")
+    employer_status: str = models.FloatField("Текущая занятость")
+
+    def __str__(self):
+        return self.user_name
+
