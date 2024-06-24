9ce5b4c1332aac434409da56bd7ef225d485b0fe
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-06-24 16:15:53 +0300

    created admin for Project, Events

--- auto_report/task/admin.py
+++ auto_report/task/admin.py
@@ -13,7 +13,7 @@ task_admin = MyAdminSite(name="task-admin")
 
 class EventInline(admin.TabularInline):
     model = Events
-    fields = ["start_time", "end_time", "event_status"]
+    fields = ["start_time", "end_time", "event_status", "creator"]
     readonly_fields = fields[:2]
     extra = False
     show_change_link = True
@@ -30,7 +30,28 @@ class TaskDataAdmin(ExportActionMixin, admin.ModelAdmin):
     list_editable = ("status",)
     search_help_text = "Поиск работает по номеру задачи или ее названию"
     inlines = (EventInline, )
+   
+class TaskInline(admin.TabularInline):
+    model = TaskData
+    fields = ["task_name", "status", "priority"]
+    readonly_fields = fields
+    extra = False
+    show_change_link = True
+
+class ProjectAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ("project_name",)
+    search_fields = ("project_name",)
+    search_help_text = "Введите название проекта"
+    inlines = (TaskInline, )
+
+
+class EventsAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ("start_time", "end_time", "event_status", "creator", )
+    list_filter = ("event_status", "creator", )
+    date_hierarchy = "start_time"
     
-task_admin.register(Project)
+    pass
+    
+task_admin.register(Project, ProjectAdmin)
 task_admin.register(TaskData, TaskDataAdmin)
-task_admin.register(Events)
+task_admin.register(Events, EventsAdmin)
