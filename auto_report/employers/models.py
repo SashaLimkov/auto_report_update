7f600a4d7414f03c7821953c89c704a881c24b02
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-06-24 16:14:56 +0300

    edited employer's model: troubles with tg_urls

--- auto_report/employers/models.py
+++ auto_report/employers/models.py
@@ -11,7 +11,8 @@ class Employer(TimeBasedModel):
         
     # мейби стоит назвать developer или employer_name
     user_name: str = models.CharField("ФИО разработчика", max_length=128)
-    tg_urls: str = models.ForeignKey(TelegramUser, on_delete=models.SET_NULL,null=True, verbose_name="Ссылка на телеграм")
+    tg_urls: str = models.CharField("Здесь должна быть ссылка на тг", max_length=128, null=True, blank=True)
+    # tg_urls: str = models.ForeignKey(TelegramUser, on_delete=models.SET_NULL,null=True, verbose_name="Ссылка на телеграм")
     start_time: str = models.DateTimeField("Начало рабочего дня")
     end_time: str = models.DateTimeField("Конец рабочего дня")
     employer_status: str = models.FloatField("Текущая занятость")
