a42131eb5794d1ecbb8e7ec1d924167e914cd83c
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-06-24 09:32:49 +0300

    requests for employers

---
+++ auto_report/employers/services/telegram_employer.py
@@ -0,0 +1,30 @@
+from telegram.models import TelegramUser
+
+
+def create_telegram_employer(
+    user_name: str, start_time: str,
+    end_time: str, employer_status: str,
+    tg_urls: str
+    ):
+    employer = TelegramUser(
+        user_name=user_name,
+        start_time=start_time,
+        end_time=end_time,
+        employer_status=employer_status,
+        tg_urls=tg_urls
+    )
+    employer.save()
+    
+    return employer
+
+
+def get_all_employers():
+    return TelegramUser.objects.all()
+
+
+def filter_employers_by_status():
+    employers = get_all_employers()
+    for employer in employers:
+        pass
+    pass
+ 
