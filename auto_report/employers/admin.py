2dc6ed5e549e642e3cecff9c727886270e7fd954
Mariya Polshkova <polshkovamamariya@gmail.com>
2024-06-24 16:59:24 +0300

    created admin for employers

--- auto_report/employers/admin.py
+++ auto_report/employers/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from task.admin import task_admin
+from task.admin import task_admin, EventInline
 from employers.models import Employer
+from rangefilter.filters import NumericRangeFilterBuilder
+from import_export.admin import ExportActionMixin
 
 
-task_admin.register(Employer)
+# class EmployerInLine(admin.TabularInline):
+#     model = Employer
+#     fields = []
+#     pass
+
+class EmployerAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ("user_name", "tg_urls", "start_time", "end_time", "employer_status", )
+    list_filter = ("user_name", "employer_status", )
+    search_fields = ("user_name", )
+    inlines = (EventInline, )
+    pass
+
+
+task_admin.register(Employer, EmployerAdmin)
